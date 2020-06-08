f26a6fbb65947b0a23bc6743860e26da169c40b7
zavalzam <zavalzam@oregonstate.edu>
2020-06-07 21:23:18 -0700

    Worked on courses

--- api/courses.js
+++ api/courses.js
@@ -1,29 +1,89 @@
 const router = require('express').Router();
 const { generateAuthToken, requireAuthentication } = require('../lib/authentication');
+const { validateAgainstSchema } = require('../lib/validation');
+const {
+  getCoursePage,
+  CourseSchema,
+  insertNewCourse,
+  getCourseDetailsById,
+  deleteCourseById,
+  getCourseStudents
+} = require('../models/course');
 
 
 
+router.get('/', async (req, res) => {
 
-router.get('/', (req, res) => {
+  try {
+    /*
+     * Fetch page info, generate HATEOAS links for surrounding pages and then
+     * send response.
+     */
+    const coursePage = await getCoursePage(parseInt(req.query.page) || 1);
+    coursePage.links = {};
+    if (coursePage.page < coursePage.totalPages) {
+      coursePage.links.nextPage = `/courses?page=${coursePage.page + 1}`;
+      coursePage.links.lastPage = `/courses?page=${coursePage.totalPages}`;
+    }
+    if (coursePage.page > 1) {
+      coursePage.links.prevPage = `/courses?page=${coursePage.page - 1}`;
+      coursePage.links.firstPage = '/courses?page=1';
+    }
+    res.status(200).send(coursePage);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: "Error fetching courses list.  Please try again later."
+    });
+  }
 
-  console.log("get a list of all courses");
 
 
 });
 
 //must be admin
-router.post('/', requireAuthentication, (req, res) => {
+router.post('/', requireAuthentication, async (req, res) => {
 
   console.log("Create a new Course");
+  if(req.role == 'admin') {
+
+    if( validateAgainstSchema(req.body, CourseSchema)  ) {
+
+      try {
+        const id = await insertNewCourse(req.body);
+        res.status(201).send({  id: id});
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: "Error inserting course into DB.  Please try again later." });
+      }
+
+    } else {
+      res.status(400).send({error: "not a valid course object"});
+    }
+
+  } else {
+    res.status(403).send({error: "must be admin to create a course"});
+  }
 
 
 });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res, next) => {
 
-  console.log("Get data on a specific course");
-
+  try {
+    const course = await getCourseDetailsById(req.params.id);
+    if (course) {
+      res.status(200).send(course);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: "Unable to fetch course.  Please try again later."
+    });
+  }
 
 });
 
@@ -36,17 +96,56 @@ router.patch('/:id', requireAuthentication, (req, res) => {
 });
 
 //must be admin
-router.delete('/:id', requireAuthentication, (req, res) => {
+router.delete('/:id', requireAuthentication, async (req, res, next) => {
 
+  if(req.role == 'admin') {
+
+    try {
+      const deleteSuccessful = await deleteCourseById(parseInt(req.params.id));
+      if (deleteSuccessful) {
+        res.status(204).end();
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        error: "Unable to delete course.  Please try again later."
+      });
+    }
+
+  } else {
+    res.status(403).send({error: "must be admin to delete a course"});
+
+  }
   console.log("Delete a course");
 
 
 });
 
 //either admin or instructor of course
-router.get('/:id/students', requireAuthentication, (req, res) => {
+router.get('/:id/students', requireAuthentication, async (req, res, next) => {
 
-  console.log("get a list of all students in course");
+  const course = await getCourseDetailsById(req.params.id);
+  if(course) {
+
+    if((course.instructorId == req.user && req.role == 'instructor') || (req.role == 'admin') ) {
+        try {
+          const students = await getCourseStudents(req.params.id);
+          res.status(200).send( {students: students} );
+        } catch (err) {
+          console.error(err);
+          res.status(500).send({
+            error: "Unable to fetch course.  Please try again later."
+          });
+        }
+    } else {
+      res.status(403).send({error: "must be admin or course instructor"});
+    }
+
+  } else {
+    next(); //course does not exist so 404
+  }
 
 
 });
@@ -60,10 +159,31 @@ router.post('/:id/students', requireAuthentication, (req, res) => {
 });
 
 //either admin or instructor of course
-router.get('/:id/roster', requireAuthentication, (req, res) => {
+router.get('/:id/roster', requireAuthentication, async (req, res) => {
 
-  console.log("get a csv of all students in the course");
+  const course = await getCourseDetailsById(req.params.id);
+  if(course) {
 
+    if((course.instructorId == req.user && req.role == 'instructor') || (req.role == 'admin') ) {
+        try {
+          const students = await getCourseStudents(req.params.id);
+
+//Do the csv shit instead of sending the array
+          res.status(200).send( {students: students} );
+
+        } catch (err) {
+          console.error(err);
+          res.status(500).send({
+            error: "Unable to fetch course.  Please try again later."
+          });
+        }
+    } else {
+      res.status(403).send({error: "must be admin or course instructor"});
+    }
+
+  } else {
+    next(); //course does not exist so 404
+  }
 
 });
 
--- models/course.js
+++ models/course.js
@@ -10,3 +10,63 @@ const CourseSchema = {
   instructor: { required: true }
 };
 exports.CourseSchema = CourseSchema;
+
+
+/*
+ * Executes a DB query to return a single page of coursees.  Returns a
+ * Promise that resolves to an array containing the fetched page of coursees.
+ */
+async function getCoursePage(page) {
+  const db = getDBReference();
+  const collection = db.collection('courses');
+  const count = await collection.countDocuments();
+
+  /*
+   * Compute last page number and make sure page is within allowed bounds.
+   * Compute offset into collection.
+   */
+  const pageSize = 5;
+  const lastPage = Math.ceil(count / pageSize);
+  page = page > lastPage ? lastPage : page;
+  page = page < 1 ? 1 : page;
+  const offset = (page - 1) * pageSize;
+
+  const results = await collection.find({})
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(pageSize)
+    .toArray();
+
+  return {
+    courses: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: pageSize,
+    count: count
+  };
+}
+exports.getCoursePage = getCoursePage;
+
+//should return id of inserted course
+async function insertNewCourse(course) {
+  const a = "write this function";
+}
+exports.insertNewCourse = insertNewCourse;
+
+
+async function getCourseDetailsById(id) {
+  const a = "write this function";
+}
+exports.getCourseDetailsById = getCourseDetailsById;
+
+
+async function deleteCourseById(id) {
+  const a = "write this function";
+}
+exports.deleteCourseById = deleteCourseById;
+
+//should return array of student ids
+async function getCourseStudents(id) {
+  const a = "write this function";
+}
+exports.getCourseStudents = getCourseStudents;
